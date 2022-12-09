3e79486a78449f3bd6e56dbe41cad8c8d081f281
vijayakumar <86522987+VJ18032000@users.noreply.github.com>
2022-12-09 16:36:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const url = "mongodb://localhost:27017/test"
+const url = "mongodb+srv://Gatello_Production:Cyb0SnTNNCWpGW2x@cluster0.wxn37.mongodb.net/test?authSource=admin&replicaSet=atlas-awavdn-shard-0&readPreference=primary&ssl=true"
 
 const app = express()
 const bodyParser=require('body-parser')
